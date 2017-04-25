e357d0852d55fd0cffb9fba9f0261962d69ba56b
Brad <bfin96@vt.edu>
2017-04-25 02:14:18 -0400

    Started to write the README.txt. After i finish with that, I will start writing tests so that I can at least get some credit for that. So far, my math is off probably and so is my color correcting probably. It crashes when you try to run scene1 and 2 because they overflow my vectors probably. However, how Scene0 is putting out right now looks cool, so thats nice I guess.

--- README.txt
+++ README.txt
@@ -1,2 +1,21 @@
 Put Your design decisions here. What modules you defined and how they
 interact to solve the ray tracing problem.
+
+vtray.cpp : This is the main program. It reads in the command line input, creates a QImage that will be read out to the file specified. It also sends the initial
+		JSON document to be parsed in the INTERPRETER class. This information is then passed to a function based on whether or not there is multithreading needed. 
+		That function then creates n number of classes that calculate the color for a single pixel and then draws it to the QImage after adjusting the color
+		and all the classes are finished running.
+
+interpreter.cpp : This is the class that parses the JSON document and throws any errors if the document will not parse correctly. It will also output the errors to the
+			standard error instead of the standard output. It also creates three objects: the Camera object, a vector with all the light sources stored in it,
+			and a vector with all the objects to be drawn in it.
+
+camera.cpp : This class is really just two structs that hold all the information needed for the camera. It stores the focus, center, normal, size, and resolution.
+
+lights.cpp : This class is really just two structs that hold all the information needed for the light source. It stores the location and intensity.
+
+shapes.cpp : This class is really just three structs that hold all the information needed for the shapes. It stores the color, center, type (sphere or plane), normal, radius, and lambert.
+
+message_queue: This is the threadsafe queue from the example code. It holds a queue of operations to be completed by the threadpool.
+
+TSwim : This is the threadpool.
--- vtray.cpp
+++ vtray.cpp
@@ -3,26 +3,23 @@
 #include <interpreter.h>
 #include <TSwim.h>
 // implement vtray main here
+using std::cout;
+using std::endl;
 
 QImage threadedDrawingandMath(int NT, interpreter interpret, QImage Picture) {
-	struct pixels {
-		std::vector<Calculate> Calcs;
-	};
-	pixels drawin;
+	
+	std::vector<Calculate> Calcs;
 	MessageQueue in, out;
 	ThreadPool P(NT, &in, &out);
 	int CamSize = interpret.Cams.CamStruct.size[0] * interpret.Cams.CamStruct.size[1];
 	for (int i = 0; i < CamSize; i++) {
-		std::cout << i << std::endl;
-		Calculate w1(interpret, CamSize);
+		Calculate w1(interpret, i);
 		in.push(&w1);
 	}
-	std::cout << "the originals went in proper" << std::endl;
 	for (int i = 0; i < NT; i++)
 	{
 		in.push(nullptr);
 	}
-	std::cout << "they all pushed in proper" << std::endl;
 	P.joinAll();
 	std::vector<double> rVals;
 	std::vector<double> gVals;
@@ -35,27 +32,77 @@ QImage threadedDrawingandMath(int NT, interpreter interpret, QImage Picture) {
 			rVals.push_back(rp->pix.pip.r);
 			gVals.push_back(rp->pix.pip.g);
 			bVals.push_back(rp->pix.pip.b);
-			drawin.Calcs.push_back(*rp);
+			Calcs.push_back(*rp);
 		}
 		else {
-			std::cout << "Unknown Work Message Type!" << std::endl;
+			std::cerr << "Error : Unknown Work Message Type!" << std::endl;
 		}
 	}
 	std::sort(rVals.begin(), rVals.end());
-	double RSCALE = rVals[rVals.size()-1];
-	double GSCALE = gVals[gVals.size() - 1];
-	double BSCALE = bVals[bVals.size() - 1];
+	std::sort(gVals.begin(), gVals.end());
+	std::sort(bVals.begin(), bVals.end());
+	double RSCALE = rVals.back();
+	double GSCALE = gVals.back();
+	double BSCALE = bVals.back();
 	std::vector<double> actual_scale{ RSCALE, GSCALE,BSCALE };
 	std::sort(actual_scale.begin(),actual_scale.end());
-	double scaler = actual_scale[actual_scale.size() - 1]/255;
-	for (size_t i = 0; i < drawin.Calcs.size(); i++)
+	double scaler = 1;
+	if(actual_scale.back() >255)
+		scaler = actual_scale.back() / 255;
+	for (size_t i = 0; i < Calcs.size(); i++)
 	{
-		double newR = drawin.Calcs[i].pix.pip.r / scaler;
-		double newG = drawin.Calcs[i].pix.pip.g / scaler;
-		double newB = drawin.Calcs[i].pix.pip.b / scaler;
-		int x = drawin.Calcs[i].pix.pip.x;
-		int y = drawin.Calcs[i].pix.pip.y;
+		double newR = Calcs[i].pix.pip.r / scaler;
+		double newG = Calcs[i].pix.pip.g / scaler;
+		double newB = Calcs[i].pix.pip.b / scaler;
+		int x = Calcs[i].pix.pip.x;
+		int y = Calcs[i].pix.pip.y;
 		uint rgb = 4278190080 + (newR * 65536) + (newG * 256) + newB;
+		Picture.setPixel(x, y, rgb);
+	}
+	return Picture;
+}
+
+QImage SingleThread(interpreter interpret, QImage Picture)
+{
+	std::vector<Calculate> Calcs;
+	std::vector<double> rVals;
+	std::vector<double> gVals;
+	std::vector<double> bVals;
+	double largest = 0;
+	int CamSize = interpret.Cams.CamStruct.size[0] * interpret.Cams.CamStruct.size[1];
+	int sizer = CamSize;
+	bool first = true;
+	for (int i = 0; i < CamSize; i++) {
+		Calculate w1(interpret, sizer);
+		w1.run();
+		sizer--;
+		w1.pix.pip.x = (i%interpret.Cams.CamStruct.size[0]);
+		w1.pix.pip.y = (i/interpret.Cams.CamStruct.size[0]);
+		Calcs.push_back(w1);
+		rVals.push_back(w1.pix.pip.r);
+		gVals.push_back(w1.pix.pip.g);
+		bVals.push_back(w1.pix.pip.b);	
+	}
+	std::sort(rVals.begin(), rVals.end());
+	std::sort(gVals.begin(), gVals.end());
+	std::sort(bVals.begin(), bVals.end());
+	double RSCALE = rVals.back();
+	double GSCALE = gVals.back();
+	double BSCALE = bVals.back();
+	std::vector<double> actual_scale{ RSCALE, GSCALE,BSCALE };
+	std::sort(actual_scale.begin(), actual_scale.end());
+	double scaler = 1;
+	if(actual_scale.back() > 255)
+		scaler = (actual_scale.back()) / 255;
+	std::cout << scaler << std::endl;
+	for (size_t i = 0; i < Calcs.size(); i++)
+	{
+		double newR = Calcs[i].pix.pip.r /scaler;
+		double newG = Calcs[i].pix.pip.g / scaler;
+		double newB = Calcs[i].pix.pip.b / scaler;
+		int x = Calcs[i].pix.pip.x;
+		int y = Calcs[i].pix.pip.y;
+		std::uint32_t rgb = 4278190080 + (newR * 65536) + (newG * 256) + newB;
 		Picture.setPixel(x, y, rgb);
 	}
 	return Picture;
@@ -64,12 +111,10 @@ QImage threadedDrawingandMath(int NT, interpreter interpret, QImage Picture) {
 int main(int argv, char * argc[]){
 	int numThreads;
 	int exit;
-	QImage picture;
 	interpreter interp;
 	QString inFile;
 	QString tds;
 	std::string multthreads;
-	
 	exit = EXIT_SUCCESS;
 	if (argv == 5)	{
 		multthreads = argc[1];
@@ -79,9 +124,12 @@ int main(int argv, char * argc[]){
 			numThreads = tds.toInt();
 			try {interp = interp.fromJSON(inFile);}
 			catch (QJsonParseError & error)	{exit = EXIT_FAILURE;}
+			QImage picture(interp.Cams.CamStruct.size[0], interp.Cams.CamStruct.size[1], QImage::Format_RGB32);
+			QString outFile= argc[4];
+			QFile out(outFile);
+			out.open(QIODevice::WriteOnly);
 			picture = threadedDrawingandMath(numThreads,interp,picture);
-			
-			picture.save("scene0", ".png", 100);
+			picture.save(&out, "PNG", 100);
 			
 		}
 		else {
@@ -93,8 +141,13 @@ int main(int argv, char * argc[]){
 		inFile = argc[1];
 		try {interp = interp.fromJSON(inFile);}
 		catch (QJsonParseError & error)	{exit = EXIT_FAILURE;}
-		picture = threadedDrawingandMath(1, interp,picture);
-		picture.save("scene0", ".png", 100);
+		QImage picture(interp.Cams.CamStruct.size[0], interp.Cams.CamStruct.size[1], QImage::Format_RGB32);
+		picture = SingleThread(interp,picture);
+		QString outFile = argc[2];
+		QFile out(outFile);
+		out.open(QIODevice::WriteOnly);
+		
+		picture.save(&out, "PNG", 100);
 	}
 	else {
 		std::cerr << "Error : Incorrect number of input arguments" << std::endl;
