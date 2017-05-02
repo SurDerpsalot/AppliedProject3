2f252a44a55e0c20e7502fa954df0d86a2406890
Brad <bfin96@vt.edu>
2017-05-02 01:02:13 -0400

    As I was trying to say in my last commit, I feel that the reason my scene 1 is so dark is because of the scale value or the exposure value. This commit includes changes to get the funcitons under the 60 line limit, even through TIDY isn't flagging those for me, which is odd. I feel like the TIDY isn't checking all of my files.

--- TSwim.cpp
+++ TSwim.cpp
@@ -47,7 +47,6 @@ Calculate::Calculate(interpreter interp, int position) {
 		double ox = origin[0];
 		double oy = origin[1];
 		double oz = origin[2];
-		//	std::cout << "X: " << origin[0] << " Y: " << origin[1] << " Z: " <<origin[2] << std::endl;
 		double rad = inter.ShapeList[i].Shps.radius;
 		double sy = inter.ShapeList[i].Shps.center.y;
 		double sx = inter.ShapeList[i].Shps.center.x;
@@ -60,22 +59,16 @@ Calculate::Calculate(interpreter interp, int position) {
 			if (!ShadowSearch) {
 				double T0, T1, Rix, Riy, Riz;
 				T0 = ((-B - sqrt(D)) / (2 * A));
-				if (T0 < 0)
-				{
+				if (T0 < 0){
 					T1 = ((-B + sqrt(D)) / (2 * A));
 					Rix = ox + xd*T1;
 					Riy = oy + yd*T1;
-					Riz = oz + zd*T1;
-				}
-				else
-				{
+					Riz = oz + zd*T1;}
+				else {
 					Rix = ox + xd*T0;
 					Riy = oy + yd*T0;
-					Riz = oz + zd*T0;
-				}
-				//std::cout << "hit" << std::endl;
-				if (pix.shadow.type == "no hit")
-				{
+					Riz = oz + zd*T0;}
+				if (pix.shadow.type == "no hit"){
 					pix.shadow.x = (Rix);// -sx) / rad;
 					pix.shadow.y = (Riy);// -sy) / rad;
 					pix.shadow.z = (Riz);// -sz) / rad;
@@ -83,10 +76,8 @@ Calculate::Calculate(interpreter interp, int position) {
 					pix.shadow.type = "sphere";
 					pix.shadow.index = i;
 					ShadowSearch = true;
-					findshadow(i);
-				}
-				else
-				{
+					findshadow(i);}
+				else {
 					std::vector<double> distOld{ pix.shadow.x - origin[0], pix.shadow.y - origin[1], pix.shadow.z - origin[2] };
 					std::vector<double> distNew{ Rix - origin[0], pix.shadow.y - origin[1], pix.shadow.z - origin[2] };
 					int old = 0;
@@ -104,37 +95,24 @@ Calculate::Calculate(interpreter interp, int position) {
 						pix.shadow.index = i;
 						pix.shadow.type = "sphere";
 						ShadowSearch = true;
-						findshadow(i);
-					}
-				}
-			}
-			else { pix.shadow.collisions.push_back(true); }
-		}
-		else {
-			if (ShadowSearch)
-				pix.shadow.collisions.push_back(false);
-		}
-	}
+						findshadow(i);}}}
+			else { pix.shadow.collisions.push_back(true); }}
+		else {if (ShadowSearch) { pix.shadow.collisions.push_back(false); }}}
 
 	void Calculate::planeintersect(int i, std::vector<double> origin, std::vector<double> destination)
 	{
 		std::vector<double> ray{ origin[0] - destination[0], origin[1] - destination[1], origin[2] - destination[2] };
 		std::vector<double> normal{ inter.ShapeList[i].Shps.normal.x,inter.ShapeList[i].Shps.normal.y ,inter.ShapeList[i].Shps.normal.z };
 		double denom = dot_product(normal, ray);
-		//std::cout << "first dot product, yay" << std::endl;
-		if (denom > 0 || denom < 0)
-		{
-			//std::cout << "made it into the if statement" << std::endl;
+		if (denom > 0 || denom < 0){
 			std::vector<double> another{ inter.ShapeList[i].Shps.center.x - origin[0],inter.ShapeList[i].Shps.center.y - origin[1] ,inter.ShapeList[i].Shps.center.z - origin[2] };
 			double t = dot_product(normal, another) / denom;
 			double Rix, Riy, Riz;
-			if (t >= 0)
-			{
+			if (t >= 0)	{
 				Rix = origin[0] + ray[0] * t;
 				Riy = origin[1] + ray[1] * t;
 				Riz = origin[2] + ray[2] * t;
-					if (!ShadowSearch)
-					{
+					if (!ShadowSearch){
 						if (pix.shadow.hit == "no hit") {
 							pix.shadow.x = (Rix);
 							pix.shadow.y = (Riy);
@@ -143,10 +121,8 @@ Calculate::Calculate(interpreter interp, int position) {
 							pix.shadow.type = "plane";
 							pix.shadow.index = i;
 							ShadowSearch = true;
-							findshadow(i);
-						}
-						else
-						{
+							findshadow(i);}
+						else{
 							std::vector<double> distOld{pix.shadow.x - origin[0], pix.shadow.y - origin[1], pix.shadow.z - origin[2]};
 							std::vector<double> distNew{Rix-origin[0], pix.shadow.y - origin[1], pix.shadow.z - origin[2]};
 							int old = 0;
@@ -164,28 +140,15 @@ Calculate::Calculate(interpreter interp, int position) {
 								pix.shadow.index = i;
 								pix.shadow.type = "plane";
 								ShadowSearch = true;
-								findshadow(i);
-							}
-						}
-					}
-					else
-					{
-						pix.shadow.collisions.push_back(true);
-					}
+								findshadow(i);}}}
+					else{ pix.shadow.collisions.push_back(true); }
 			}
-			else
-			{
-				if (ShadowSearch) {
-					pix.shadow.collisions.push_back(false);
-				}
+			else {
+				if (ShadowSearch) { pix.shadow.collisions.push_back(false); }
 			}
-
 		}
-		else
-		{
-			if (ShadowSearch) {
-				pix.shadow.collisions.push_back(false);
-			}
+		else{
+			if (ShadowSearch) {	pix.shadow.collisions.push_back(false); }
 		}
 	}
 
@@ -203,63 +166,45 @@ Calculate::Calculate(interpreter interp, int position) {
 		if (inter.ShapeList[w].Shps.type == "plane") {
 			origin[0] = pix.shadow.x - inter.ShapeList[w].Shps.center.x;
 			origin[1] = pix.shadow.y - inter.ShapeList[w].Shps.center.y;
-			origin[2] = pix.shadow.z - inter.ShapeList[w].Shps.center.z;
-		}
-		for (size_t j = 0; j < inter.LightList.size(); j++)
-		{
+			origin[2] = pix.shadow.z - inter.ShapeList[w].Shps.center.z;}
+		for (size_t j = 0; j < inter.LightList.size(); j++) {
 			dest.push_back(inter.LightList[j].lite.location.x);
 			dest.push_back(inter.LightList[j].lite.location.y);
 			dest.push_back(inter.LightList[j].lite.location.z);
-			for (size_t i = 0; i < inter.ShapeList.size(); i++)
-			{
-				if (inter.ShapeList[i].Shps.type == "sphere")
-				{
+			for (size_t i = 0; i < inter.ShapeList.size(); i++)	{
+				if (inter.ShapeList[i].Shps.type == "sphere"){
 					sphereintersect(i, origin, dest);
-					if (pix.shadow.collisions.back())
-					{
+					if (pix.shadow.collisions.back()){
 						pix.shadow.collisions.pop_back();
 						pix.pip.r = pix.pip.r + 0;
 						pix.pip.g = pix.pip.g + 0;
-						pix.pip.b = pix.pip.b + 0;
-					}
-					else
-					{
+						pix.pip.b = pix.pip.b + 0;	}
+					else{
 						if (pix.shadow.type == "sphere") {
 							pix.shadow.collisions.pop_back();
 							nx = (origin[0] - inter.ShapeList[i].Shps.center.x) / inter.ShapeList[i].Shps.radius;
 							ny = (origin[1] - inter.ShapeList[i].Shps.center.y) / inter.ShapeList[i].Shps.radius;
-							nz = (origin[2] - inter.ShapeList[i].Shps.center.y) / inter.ShapeList[i].Shps.radius;
-						}
-					}
-				}
-				else
-				{
+							nz = (origin[2] - inter.ShapeList[i].Shps.center.y) / inter.ShapeList[i].Shps.radius;}}}
+				else{
 					planeintersect(i, dest, origin);
-					if (pix.shadow.collisions.back())
-					{
+					if (pix.shadow.collisions.back()){
 						pix.shadow.collisions.pop_back();
 						pix.pip.r = pix.pip.r + 0;
 						pix.pip.g = pix.pip.g + 0;
-						pix.pip.b = pix.pip.b + 0;
-					}
-					else
-					{
+						pix.pip.b = pix.pip.b + 0;}
+					else{
 						if (pix.shadow.type == "plane") {
 							pix.shadow.collisions.pop_back();
 							nx = origin[0] - inter.ShapeList[i].Shps.center.x;
 							ny = origin[1] - inter.ShapeList[i].Shps.center.y;
-							nz = origin[2] - inter.ShapeList[i].Shps.center.z;
-						}
-					}
-				}
+							nz = origin[2] - inter.ShapeList[i].Shps.center.z;}}}
 					std::vector<double> Normal{ nx,ny,nz };
 					std::vector<double> shadow{ dest[0] - pix.shadow.x,dest[1] - pix.shadow.y,dest[2] - pix.shadow.z };
 					scale = dot_product(Normal, shadow) * inter.ShapeList[i].Shps.lambert;
 					if (scale < 0) { scale = 0; }
 					pix.pip.r = pix.pip.r + (scale *inter.LightList[j].lite.intensity * inter.ShapeList[i].Shps.color.r);
 					pix.pip.g = pix.pip.g + (scale *inter.LightList[j].lite.intensity * inter.ShapeList[i].Shps.color.g);
-					pix.pip.b = pix.pip.b + (scale *inter.LightList[j].lite.intensity * inter.ShapeList[i].Shps.color.b);
-			}
+					pix.pip.b = pix.pip.b + (scale *inter.LightList[j].lite.intensity * inter.ShapeList[i].Shps.color.b);}
 		}
 }
 
